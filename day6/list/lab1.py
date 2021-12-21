6792444db77599f5f9272a66376c5c3b9af21086
unknown <kumaritulasi97@gmail.com>
2021-12-21 16:44:15 +0545

    try

---
+++ day6/list/lab1.py
@@ -0,0 +1,6 @@
+# program to sum all the items in a list. 
+list=[1,2,3,4,5]
+sum=0
+for i in list:
+    sum+=i
+print(sum)
---
+++ day6/list/lab2.py
@@ -0,0 +1,6 @@
+# program to multiplies all the items in a list. 
+list=[1,2,3,4,5]
+multiple=1
+for i in list:
+    multiple*=i
+print(multiple)
